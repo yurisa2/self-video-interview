e75f73d3effc5f93e5793f5fc0d458f7ca5f42ad
Yuri Sá <yuri@sa2.com.br>
2024-12-28 15:45:53 +0000

    Added some error descriptions and some failsafes for the video sending

--- pages/Interview/jsobjects/video_ops/video_ops.js
+++ pages/Interview/jsobjects/video_ops/video_ops.js
@@ -1,14 +1,27 @@
 export default {
 	
 	async on_finish_camera () {
-		await storeValue("response_message", "")
-		await clearInterval("q1timer")
-		await showModal(mdl_wait.name)
-		await post_video.run()
-		await storeValue("response_message", post_video.data.message)
-		await closeModal(mdl_wait.name)
-		await console.log(post_video.data.message)
-		await navigateTo("Final")
 		
+		if(Camera1.imageDataURL === "undefined") {
+			showAlert("No video found, record it again")
+		}
+		else {
+			await storeValue("response_message", "")
+			await clearInterval("q1timer")
+			await showModal(mdl_wait.name)
+
+				try {
+					await post_video.run()
+					await storeValue("response_message", post_video.data.message)
+					await closeModal(mdl_wait.name)
+					await console.log(post_video.data.message)
+					await navigateTo("Final")
+				} catch (error) {
+					console.error("Error uploading video:", error);
+					showAlert("Error uploading video", "error")
+					Text5.setText("ERROR SENDING YOU VIDEO, TRY AGAIN")
+					Text5Copy.setText("Currently we are not compressing the video (this is a PoC), so it can be tough on your network, try shorter videos. Click out side this screen and try again")
+				}
+			}
 	},	
 }
